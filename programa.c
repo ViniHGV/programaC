d40ab229f0b0c454675c0a892eb23ea08a3f0554
vinicius <ixxvinicius@gmail.com>
2021-11-26 14:51:15 -0300

    first commit

---
+++ programa.c
@@ -0,0 +1,12 @@
+
+syntax on
+set nu
+#include<stdio.h>
+#include<stdlib.h>
+int main(void){
+	printf("boa tarde");
+	printf("\n.\n");
+	return 0;
+}
+
+
---
+++ programinha.c
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void main() {
+float a, b, c;
+printf("\ninforme um número: ");
+scanf("%f", &a);
+
+if(a>10)
+	{
+	printf("\nValor digita é maior que 10");
+	}
+		else
+		{
+		printf("\nValor digitado é menor ou igual a 10\n");
+	 	}
+}
